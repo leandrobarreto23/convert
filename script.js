9f8e4ce39a6d58c5d803a3c25545cc1d3722ac24
Leandro Barreto <leandro.barreto.lb23@outlook.com>
2025-02-14 15:55:32 -0300

    obtendo a moeda selecionada

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
 
 // Manipulando o input amount para receber somente numeros
 amount.addEventListener("input", () => {
-  
-  
   const hasCharactersRegex = /\D+/g
   amount.value = amount.value.replace(hasCharactersRegex,"")
 })
+
+// Capturando o evento de submit no form
+form.onsubmit = (e) => {
+  e.preventDefault()
+
+  console.log(currency.value)
+}
